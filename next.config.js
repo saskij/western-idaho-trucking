f5811cf17b680999c78a504aa18cf9295b21e008
Alexander Ashurov <bstuashurov@gmail.com>
2026-02-24 17:52:22 -0700

    Update Next.js config with basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/western-idaho-trucking',
+    assetPrefix: '/western-idaho-trucking',
     images: {
         unoptimized: true,
     },
